500ea6e2bae63a88d5219e2457a2d8dfa619ca2e
ujjwal1801 <sharmaujjwal180@gmail.com>
2017-06-24 15:04:36 +0530

    Add:Project Files

---
+++ app/src/main/java/com/example/android/skyer/Skyer.java
@@ -0,0 +1,13 @@
+package com.example.android.skyer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Skyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skyer);
+    }
+}
